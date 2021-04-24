05793461768db884f07730b2ec9ec8c74f5f4b37
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-04-24 19:06:03 +0300

    Basic Toolbar (temporary)

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/MainActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/MainActivity.java
@@ -1,14 +1,65 @@
 package com.craftbid.craftbid;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    public boolean logged_in = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set custom support action bar
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.app_bar);
+        getSupportActionBar().setElevation(0);
+
+//        //Set Back Arrow
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        //Change Toolbar Title
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        toolbar.setTitle("feed");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbarmenu, menu);
+        if(logged_in){
+            menu.findItem(R.id.login).setVisible(false);
+            menu.findItem(R.id.signup).setVisible(false);
+        }else{
+            menu.findItem(R.id.logout).setVisible(false);
+            menu.findItem(R.id.profile).setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.login:
+            case R.id.signup:
+                logged_in = true;
+                break;
+            case R.id.logout:
+                logged_in = false;
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        invalidateOptionsMenu();
+        return true;
+    }
+
 }
