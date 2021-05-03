145243f375c89f722782c752b13f577190d993ea
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-05-04 00:10:41 +0300

    Partial Signup Customer Activity

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/LoginActivity.java
@@ -2,7 +2,9 @@ package com.craftbid.craftbid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void openSignupCustomer(View view) {
+        Intent signupCustomer = new Intent(LoginActivity.this, SignupCustomerActivity.class);
+        startActivity(signupCustomer);
     }
 }
