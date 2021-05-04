02983616b73926afb2a9faf7538a8008d049edbc
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-05-04 17:26:23 +0300

    AppBar as different layout

---
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/CustomerProfile.java
@@ -0,0 +1,14 @@
+package com.craftbid.craftbid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_profile);
+    }
+}
