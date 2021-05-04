8ff90b5744b7176381dbf368f02b309588b0d17e
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-05-04 16:51:37 +0300

    Sign up customer complete

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/SignupCustomerActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/SignupCustomerActivity.java
@@ -3,6 +3,8 @@ package com.craftbid.craftbid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class SignupCustomerActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SignupCustomerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_customer);
+
+        TextView password = findViewById(R.id.password_label);
+        String star = getColoredSpanned("*", String.valueOf(getResources().getColor(R.color.error)));
+        String text = getColoredSpanned(getResources().getString(R.string.password_en), "fff");
+        password.setText(Html.fromHtml(star+text));
+    }
+    private String getColoredSpanned(String text, String color) {
+        String input = "<font color=" + color + ">" + text + "</font>";
+        return input;
     }
 }
