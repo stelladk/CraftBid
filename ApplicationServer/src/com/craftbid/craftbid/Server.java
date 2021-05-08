6683f870f6b5612878c34e697c48cde19b4e27c8
k-souvatzidaki <kwn.souv@gmail.com>
2021-05-08 17:59:11 +0300

    Test connection to db

--- ApplicationServer/src/com/craftbid/craftbid/Server.java
+++ ApplicationServer/src/com/craftbid/craftbid/Server.java
@@ -2,11 +2,13 @@ package com.craftbid.craftbid;
 
 import java.net.*;
 import java.io.*;
+import java.sql.*;
 
 public class Server {
 
     String ip; int port,concurrent_requests;
     ServerSocket requests;
+
 
     public Server(String ip,int port,int concurrent_requests) {
         System.out.println("Starting server in ip "+ip+" and port "+port);
@@ -63,6 +65,21 @@ public class Server {
     }
 
     public static void main(String[] args) {
-        new Server("192.168.2.2",6500,100);
+        //new Server("192.168.2.2",6500,100);
+
+        //load jdbc driver
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //test connection to database
+            Connection db_connect = null;
+            String db_url = "jdbc:sqlserver://"+Constants.HOST+":"+Constants.PORT+";databaseName="+Constants.DATABASE;
+            db_connect = DriverManager.getConnection(db_url,Constants.USER,Constants.PASSWORD);
+
+            String query = "SELECT * FROM Creator;";
+            Statement stm = db_connect.createStatement();
+            ResultSet res = stm.executeQuery(query);
+        }catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
