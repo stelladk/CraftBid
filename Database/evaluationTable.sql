adf283d28c3949c72013d926194a2c7ca772ad6c
themelina <melinas792@gmail.com>
2021-05-09 16:10:34 +0300

    Evaluations table added

---
+++ Database/evaluationTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Evaluation (
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	submitted_by VARCHAR(20) FOREIGN KEY REFERENCES UserInfo(username) NOT NULL,
+	refers_to VARCHAR(20) FOREIGN KEY REFERENCES UserInfo(username) NOT NULL,
+	rating INT NOT NULL,
+	date DATE NOT NULL
+);
