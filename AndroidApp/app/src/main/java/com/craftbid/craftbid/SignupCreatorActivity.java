4e2eea4b78157f572ce7d7b209321a3d2235f2b1
k-souvatzidaki <kwn.souv@gmail.com>
2021-05-19 21:39:11 +0300

    Start signup AsyncTask

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/SignupCreatorActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/SignupCreatorActivity.java
@@ -2,15 +2,20 @@ package com.craftbid.craftbid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,11 +23,14 @@ import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.*;
+import java.net.Socket;
 
 public class SignupCreatorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private String expertise_selected;
     private final int PHOTO_PICK = 1;
+    byte[] buffer = null;
+    boolean is_successful = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +61,10 @@ public class SignupCreatorActivity extends AppCompatActivity implements AdapterV
     }
 
     public void attemptSighup(View view) {
-        //TODO
-        //Do actions to confirm sign up
-        Intent main = new Intent(SignupCreatorActivity.this, MainActivity.class);
-        startActivity(main);
+        //Start AsyncTask for signup
+        new SignUpTask().execute("");
+
+
     }
 
     @Override
@@ -70,10 +78,9 @@ public class SignupCreatorActivity extends AppCompatActivity implements AdapterV
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
-    
+
+    //click on imageview to get an image from gallery
     public void onImageClick(View view) {
-        Snackbar.make(view, "Click! " , Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
         Intent photo_picker = new Intent(Intent.ACTION_PICK,
                                          android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         photo_picker.setType("image/*");
@@ -83,26 +90,86 @@ public class SignupCreatorActivity extends AppCompatActivity implements AdapterV
     @Override
     protected void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            try {
-                Uri uri = data.getData();
-                InputStream in = getContentResolver().openInputStream(uri);
-                Bitmap photo = BitmapFactory.decodeStream(in);
-                ImageView photo_profile= (ImageView) findViewById(R.id.photo_profile);
-                photo_profile.setImageBitmap(photo);
-                //convert photo to byte array to send to server
-                ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-                byte[] buffer = new byte[1024];
-                int len = 0;
-                while ((len = in.read(buffer)) != -1) {
-                    byteBuffer.write(buffer, 0, len);
+        if(reqCode == PHOTO_PICK) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    Uri uri = data.getData();
+                    InputStream in = getContentResolver().openInputStream(uri);
+                    Bitmap photo = BitmapFactory.decodeStream(in);
+                    ImageView photo_profile= (ImageView) findViewById(R.id.photo_profile);
+                    photo_profile.setImageBitmap(photo);
+                    //convert photo to byte array to send to server
+                    ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+                    buffer = new byte[1024];
+                    int len = 0;
+                    while ((len = in.read(buffer)) != -1) {
+                        byteBuffer.write(buffer, 0, len);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
+
+            }else {
+
+            }
+        }
+    }
+
+    //AsyncTask running when signup button is clicked, connecting to server to signup
+    private class SignUpTask extends AsyncTask<String, String, Void> {
+        ProgressDialog progressDialog;
+        String resultmsg = null;
+        @Override
+        protected Void doInBackground(String... params) {
+            Socket socket = null;
+            ObjectOutputStream out = null;
+            ObjectInputStream in = null;
+            String response;
+            //get values from input fields
+            String name = ((EditText)findViewById(R.id.fullname_edit)).getText().toString();
+            String email = ((EditText)findViewById(R.id.email_edit)).getText().toString();
+            String phone = ((EditText)findViewById(R.id.phone_edit)).getText().toString();
+            boolean freelancer = ((CheckBox)findViewById(R.id.freelancer)).isChecked();
+            String category = ((Spinner)findViewById(R.id.expertise_spinner)).getSelectedItem().toString();
+            String username = ((EditText)findViewById(R.id.username_edit)).getText().toString();
+            String password = ((EditText)findViewById(R.id.password_edit)).getText().toString();
+            String repeat_password = ((EditText)findViewById(R.id.repeat_password_edit)).getText().toString();
+
+
+            //validation checks
+            if(name.equals("")|| email.equals("") || username.equals("") || password.equals("") || repeat_password.equals("")) {
+                resultmsg = "Συμπληρώστε τα υποχρεωτικά πεδία";
+            }else if(!password.equals(repeat_password)) {
+                resultmsg = "Οι κωδικοί δεν ταιριάζουν";
+            }else {
+                is_successful = true;
+            }
+            /*
+            try {
+                socket = new Socket("192.168.2.2",6500);
+            }catch(IOException e) {
                 e.printStackTrace();
             }
+             */
+            return null;
+        }
 
-        }else {
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(SignupCreatorActivity.this,
+                    "Sign Up...",
+                    "Connecting to server...");
+        }
 
+        @Override
+        protected void onPostExecute(Void result) {
+            progressDialog.dismiss();
+            Snackbar.make( getWindow().getDecorView().getRootView(), resultmsg , Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            if(is_successful) {
+                Intent login = new Intent(SignupCreatorActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
         }
     }
 }
