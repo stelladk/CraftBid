873fcaee571b158c70accfd1f6b6398fbc37d604
k-souvatzidaki <kwn.souv@gmail.com>
2021-05-20 12:50:38 +0300

    Remove photo columns from db

---
+++ Database/removePhotoColumnsUnneeded.sql
@@ -0,0 +1,9 @@
+ALTER TABLE Listing DROP COLUMN thumbnail;
+
+ALTER TABLE Notification DROP COLUMN photo;
+
+ALTER TABLE Reward DROP COLUMN photo;
+
+ALTER TABLE UserInfo DROP COLUMN photo;
+
+--TODO remove table Photo
