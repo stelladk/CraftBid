3a8da4915cc2ec2f67ba7996ece0915a264971a8
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-05-25 15:11:39 +0300

    Listing backend | have not tested

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/ListingPublicActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/ListingPublicActivity.java
@@ -1,21 +1,39 @@
 package com.craftbid.craftbid;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.craftbid.craftbid.model.Listing;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ListingPublicActivity extends AppCompatActivity {
     private int listing_id;
     private static String previous;
     Dialog dialog;
+
+    public static Listing listing;
+    public static ArrayList<byte[]> listing_photos;
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -28,6 +46,8 @@ public class ListingPublicActivity extends AppCompatActivity {
             previous = b.getString("previous", previous);
         }
         Log.d("ListingPublic", "onCreate: listing_id "+listing_id);
+
+        new LoadListingTask().execute(); //Load listing info
 
         if(previous.equals(MainActivity.GUEST)){
             findViewById(R.id.creator_btn).setEnabled(false);
@@ -88,5 +108,61 @@ public class ListingPublicActivity extends AppCompatActivity {
         dialog.show();
 
         dialog.findViewById(R.id.close_btn).setOnClickListener(v -> dialog.dismiss());
+    }
+
+    private class LoadListingTask extends AsyncTask<Void, Void, Void>{
+        ProgressDialog progressDialog;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Socket socket = null;
+            ObjectOutputStream out = null;
+            ObjectInputStream in = null;
+            //connect to server to loa listing info
+            try {
+                socket = new Socket("192.168.2.2",6500);
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject("VIEW_LISTING");
+                out.writeObject(listing_id);
+
+                listing = (Listing)in.readObject();
+                listing_photos = (ArrayList<byte[]>) in.readObject();
+            }catch(IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(ListingPublicActivity.this,
+                    "View Listing...",
+                    "Connecting to server...");
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressDialog.dismiss();
+            if(listing != null) {
+                TextView name = findViewById(R.id.listing_name);
+                name.setText(listing.getName());
+                Button creator_btn = findViewById(R.id.creator_btn);
+                creator_btn.setText(listing.getPublished_by());
+                TextView details = findViewById(R.id.details);
+                details.setText(listing.getDescription());
+                TextView location = findViewById(R.id.location);
+                location.setText(listing.getLocation());
+                TextView value = findViewById(R.id.points_value);
+                value.setText(listing.getReward_points());
+                TextView price = findViewById(R.id.price);
+                price.setText(String.format("%s", listing.getMin_price()));
+                if(listing_photos!=null && listing_photos.size()>0){
+                    ImageView photo = findViewById(R.id.listing_photo);
+                    Bitmap thumbnail = BitmapFactory.decodeByteArray(listing_photos.get(0),0, listing_photos.get(0).length);
+                    photo.setImageBitmap(thumbnail);
+                }
+            }
+        }
     }
 }
--- AndroidApp/app/src/main/java/com/craftbid/craftbid/MakeOfferActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/MakeOfferActivity.java
@@ -1,11 +1,16 @@
 package com.craftbid.craftbid;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,8 +18,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.craftbid.craftbid.model.Listing;
+import com.craftbid.craftbid.model.Offer;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class MakeOfferActivity extends AppCompatActivity {
     private int listing_id;
+    private Listing listing;
+    private ArrayList<byte[]> listing_photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +39,13 @@ public class MakeOfferActivity extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
         if(b!=null){
-            listing_id = b.getInt("listing_id");
+            listing_id = b.getInt("listing_id"); //Do we need this?
+        }
+
+        if(ListingPublicActivity.listing != null){
+            listing = ListingPublicActivity.listing;
+            listing_photos = ListingPublicActivity.listing_photos;
+            loadListing();
         }
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -58,6 +80,26 @@ public class MakeOfferActivity extends AppCompatActivity {
         startActivity(listing);
     }
 
+    private void loadListing(){
+        TextView name = findViewById(R.id.listing_name);
+        name.setText(listing.getName());
+        Button creator_btn = findViewById(R.id.creator_btn);
+        creator_btn.setText(listing.getPublished_by());
+        TextView details = findViewById(R.id.details);
+        details.setText(listing.getDescription());
+        TextView location = findViewById(R.id.location);
+        location.setText(listing.getLocation());
+        TextView value = findViewById(R.id.points_value);
+        value.setText(listing.getReward_points());
+        TextView price = findViewById(R.id.price);
+        price.setText(String.format("%s", listing.getMin_price()));
+        if(listing_photos!=null && listing_photos.size()>0){
+            ImageView photo = findViewById(R.id.listing_photo);
+            Bitmap thumbnail = BitmapFactory.decodeByteArray(listing_photos.get(0),0, listing_photos.get(0).length);
+            photo.setImageBitmap(thumbnail);
+        }
+    }
+
     // TODO
     public void viewPhotos(View view) {
         Log.d("photos", "Viewing photos");
@@ -65,6 +107,44 @@ public class MakeOfferActivity extends AppCompatActivity {
 
     public void submitOffer(View view) {
         Log.d("submit", "Submit offer");
-        goBack();
+        new SubmitTask().execute();
+    }
+
+    private class SubmitTask extends AsyncTask<Void, Void, Void>{
+        ProgressDialog progressDialog;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Socket socket = null;
+            ObjectOutputStream out = null;
+            ObjectInputStream in = null;
+            //connect to server to loa listing info
+            try {
+                socket = new Socket("192.168.2.2",6500);
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject("CREATE_BID");
+                //Create Offer object
+                float price = Float.parseFloat(((EditText)findViewById(R.id.offer)).getText().toString());
+                Offer bid = new Offer(-1, listing.getId(), MainActivity.username, price);
+                out.writeObject(bid);
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(MakeOfferActivity.this,
+                    "Submit Offer...",
+                    "Connecting to server...");
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressDialog.dismiss();
+            goBack();
+        }
     }
 }
