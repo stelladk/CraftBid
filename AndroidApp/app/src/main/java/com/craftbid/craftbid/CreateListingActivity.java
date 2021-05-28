57e30c20a2ebf2410eef0c963cbabb7af3ac2c75
Stella Douka <35050451+stelladk@users.noreply.github.com>
2021-05-28 19:37:53 +0300

    Fix date bug

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/CreateListingActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/CreateListingActivity.java
@@ -1,12 +1,5 @@
 package com.craftbid.craftbid;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -21,13 +14,17 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.craftbid.craftbid.adapters.CollectionRecyclerAdapter;
-import com.craftbid.craftbid.adapters.FeedRecyclerAdapter;
 import com.craftbid.craftbid.model.Listing;
-import com.craftbid.craftbid.model.Thumbnail;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.ByteArrayOutputStream;
@@ -36,9 +33,11 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Locale;
 
 public class CreateListingActivity extends AppCompatActivity {
     private final int PHOTO_PICK = 1;
@@ -155,9 +154,9 @@ public class CreateListingActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            Socket socket = null;
-            ObjectOutputStream out = null;
-            ObjectInputStream in = null;
+            Socket socket;
+            ObjectOutputStream out;
+            ObjectInputStream in;
 
             //get values from input fields
             //TODO check if they are empty
@@ -165,10 +164,20 @@ public class CreateListingActivity extends AppCompatActivity {
             String description = ((EditText)findViewById(R.id.description_edit)).getText().toString();
             String category = ((Spinner)findViewById(R.id.listing_category)).getSelectedItem().toString();
             String location = ((Spinner)findViewById(R.id.location_spinner)).getSelectedItem().toString();
-            int points = getEditTextValue((EditText)findViewById(R.id.points_edit), 0);
-            int quantity = getEditTextValue((EditText)findViewById(R.id.quantity_edit), 1);
-            float min_price = getEditTextValue((EditText)findViewById(R.id.init_price_edit), 0.0F);
+            int points = getEditTextValue(findViewById(R.id.points_edit), 0);
+            int quantity = getEditTextValue(findViewById(R.id.quantity_edit), 1);
+            float min_price = getEditTextValue(findViewById(R.id.init_price_edit), 0.0F);
             String date = String.valueOf(new Date()); //TODO take date format from database
+            SimpleDateFormat inputFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy", Locale.ENGLISH);
+            SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            Date formattedDate;
+            try {
+                formattedDate = inputFormat.parse(date);
+                date = outputFormat.format(formattedDate);
+                Log.d("CREATE LISTING", "doInBackground: DATE "+date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             boolean shipment = ((CheckBox)findViewById(R.id.shipment_check)).isChecked();
             boolean handin = ((CheckBox)findViewById(R.id.handin_check)).isChecked();
             String delivery = (shipment? "Ταχυδρομικά":"") + ", " + (handin? "Χέρι-με-χέρι":"");
