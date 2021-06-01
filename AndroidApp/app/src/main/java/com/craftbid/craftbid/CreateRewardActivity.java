fec5bfde30a2de6a1a4a2cfcd3120b1712e55f85
themelinaKz <melinas792@gmail.com>
2021-06-01 16:16:38 +0300

    Create reward connection to server done(server last response missing)

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/CreateRewardActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/CreateRewardActivity.java
@@ -12,12 +12,16 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.craftbid.craftbid.model.Listing;
+import com.craftbid.craftbid.model.Reward;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.ByteArrayOutputStream;
@@ -94,7 +98,77 @@ public class CreateRewardActivity extends AppCompatActivity {
 
     public void saveReward(View view) {
         Log.d("saveReward", "saved");
-        // TODO check if everything is okay and then call AsyncTask
-        //new CreateRewardTask().execute();
+        new CreateRewardTask().execute();
+    }
+
+    private class CreateRewardTask extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog progressDialog;
+        Socket socket = null;
+        ObjectOutputStream out = null;
+        ObjectInputStream in = null;
+        String response, resultmsg;
+        boolean is_successful;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(CreateRewardActivity.this,
+                    "Create reward...",
+                    "Connecting to server...");
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            //get values from input fields
+            String reward_name = ((EditText) findViewById(R.id.reward_name)).getText().toString();
+            String points_str = ((EditText) findViewById(R.id.set_reward_points)).getText().toString();
+            int points = Integer.parseInt((points_str.equals("") ? "0" : points_str));
+
+            if(reward_name.equals("") || points==0){
+                resultmsg= "Συμπληρώστε όνομα βραβείου και πόντους!";
+                return null;
+            }
+            if(buffer == null){
+                resultmsg = "Πρέπει να προσθέσετε φωτογραφία!";
+                return null;
+            }
+            // Reward object
+            Reward reward = new Reward(-1,points, reward_name, MainActivity.username, buffer);
+
+            //connect to server to add reward
+            try {
+                socket = new Socket(NetInfo.getServer_ip(),NetInfo.getServer_port());
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject("ADD_REWARD");
+                out.writeObject(reward);
+                out.writeObject(buffer);
+
+                /* TODO uncomment when AppServer sends response
+                response = (String)in.readObject();
+                if(response.equals("ADD REWARD SUCCESSFUL")) {
+                    resultmsg = "Η προσθήκη του βραβείου ήταν επιτυχής!";
+                    is_successful = true;
+                }
+                */
+            }catch(IOException /*TODO also uncomment| ClassNotFoundException*/ e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressDialog.dismiss();
+            Snackbar.make( getWindow().getDecorView().getRootView(), resultmsg , Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            try {
+                if(socket!=null) socket.close();
+                if(out!=null) out.close();
+                if(in!=null) in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(is_successful) goBack();
+        }
     }
 }
