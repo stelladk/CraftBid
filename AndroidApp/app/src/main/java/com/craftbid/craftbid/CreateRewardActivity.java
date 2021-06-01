ec8a780fc9dd97d55cdbf75aaf9b0cc36f484e44
themelinaKz <melinas792@gmail.com>
2021-06-01 14:30:36 +0300

    Upload image for reward done (image_item in xml replaced)

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/CreateRewardActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/CreateRewardActivity.java
@@ -1,17 +1,36 @@
 package com.craftbid.craftbid;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class CreateRewardActivity extends AppCompatActivity {
+
+    private static final int PHOTO_PICK = 1;
+    byte[] buffer = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,15 +44,38 @@ public class CreateRewardActivity extends AppCompatActivity {
         //Set Back Arrow
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
 
-        findViewById(R.id.reward_photo).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Log.d("addPhoto", "CreateRewardActivity");
-                // TODO open phone's collection and pick photo
-                return true;
+    /** Opens gallery to pick a photo for reward */
+    public void onImageClick(View view){
+        Intent photo_picker = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        photo_picker.setType("image/jpeg");
+        startActivityForResult(photo_picker,PHOTO_PICK);
+    }
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        Log.d("MOUA", "in activityResult");
+        if(reqCode == PHOTO_PICK) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    Uri uri = data.getData();
+                    InputStream in = getContentResolver().openInputStream(uri);
+                    Bitmap photo = BitmapFactory.decodeStream(in);
+                    ImageView photo_profile= (ImageView) findViewById(R.id.reward_photo);
+                    photo_profile.setImageBitmap(photo);
+                    //convert photo to byte array to send to server
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    photo.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+                    buffer = stream.toByteArray();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                Log.d("error","no image given");
             }
-        });
+        }
     }
 
     @Override
@@ -52,5 +94,7 @@ public class CreateRewardActivity extends AppCompatActivity {
 
     public void saveReward(View view) {
         Log.d("saveReward", "saved");
+        // TODO check if everything is okay and then call AsyncTask
+        //new CreateRewardTask().execute();
     }
 }
