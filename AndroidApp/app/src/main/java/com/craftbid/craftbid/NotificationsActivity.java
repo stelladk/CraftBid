a2af06613a52d98e22aa005be2bbada2823890d1
themelinaKz <melinas792@gmail.com>
2021-06-02 20:17:49 +0300

    Notifications activity request to server (not tested)

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/NotificationsActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/NotificationsActivity.java
@@ -7,49 +7,53 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.craftbid.craftbid.adapters.FeedRecyclerAdapter;
 import com.craftbid.craftbid.adapters.NotificationsAdapter;
+import com.craftbid.craftbid.adapters.OffersRecyclerAdapter;
+import com.craftbid.craftbid.model.Notification;
+import com.craftbid.craftbid.model.Offer;
 import com.craftbid.craftbid.model.Thumbnail;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class NotificationsActivity extends AppCompatActivity {
-    private String username;
+    private NotificationsAdapter adapter;
+    private RecyclerView recycler;
+    private ArrayList<Notification> notifications;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
-        username = MainActivity.username;
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Ειδοποιήσεις");
 
         //Set Back Arrow
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        byte[] test = new byte[2];
-        List<Thumbnail> notifications = new ArrayList<>();
-        notifications.add(new Thumbnail(0, "Πλεκτή τσάντα", "Ωραιότατη πλεκτή τσάντα πάρε πάρε όλα 5 ευρώ αρχική","Πλεκτά, Τσάντες", 18, test));
-        notifications.add(new Thumbnail(1, "Βραχιόλια", "Βραχιολι χειροποιητο αν θες το παιρνεις", "Κοσμήματα",  5, test));
-        notifications.add(new Thumbnail(2, "Πλεκτά για όλους", "Πλεκτά ρούχα για όλες τις ηλικίε δεχόμαστε παραγγελίες", "Πλεκτά",  15, test));
-        notifications.add(new Thumbnail(3, "Πασχαλινό κερί χειροποίητο", "Κεριά Πασχαλινά για την Ανάσταση. Χρόνια Πολλά!", "Κεριά", 20, test));
-        notifications.add(new Thumbnail(4, "Ξύλινη Καρέκλα", "Ωραιότατη Ξύλινη Καρέκλα", "Καρέκλες", 15, test));
-        notifications.add(new Thumbnail(5, "Ξύλινη Καρέκλα", "Ξύλινη Καρέκλα Κήπου", "Καρέκλες", 20, test));
-        notifications.add(new Thumbnail(6, "Πλεκτή τσάντα", "Ωραιότατη πλεκτή τσάντα πάρε πάρε όλα 5 ευρώ αρχική","Πλεκτά, Τσάντες", 12, test));
-        notifications.add(new Thumbnail(7, "Πασχαλινό κερί χειροποίητο", "Κεριά Πασχαλινά για την Ανάσταση. Χρόνια Πολλά!", "Κεριά", 7, test));
-
-        RecyclerView recycler = findViewById(R.id.notifications_recyclerview);
+        recycler = findViewById(R.id.notifications_recyclerview);
         RecyclerView.LayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recycler.setLayoutManager(manager);
-        NotificationsAdapter adapter = new NotificationsAdapter(notifications, this);
-        recycler.setAdapter(adapter);
     }
 
     @Override
@@ -61,20 +65,63 @@ public class NotificationsActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
     private void goBack() {
         Intent main = new Intent(NotificationsActivity.this, MainActivity.class);
         startActivity(main);
     }
 
-    //Temporary
-    public int getDrawable(String name) {
-        return this.getResources().getIdentifier(name, "drawable", this.getPackageName());
+    public void purchase(Notification notification) {
+        Intent purchase = new Intent(NotificationsActivity.this, PurchaseActivity.class);
+        purchase.putExtra("listing_id", notification.getListing_id());
+        startActivity(purchase);
     }
 
-    public void purchase(Thumbnail notification) {
-        Intent purchase = new Intent(NotificationsActivity.this, PurchaseActivity.class);
-        purchase.putExtra("listing_id", notification.getId());
-        startActivity(purchase);
+    // TODO waiting for server's correction to be tested
+    /** On creation of screen, connects to server to get list of Offers for particular listing*/
+    private class LoadNotificationsTask extends AsyncTask<Void, Void, Void> {
+        ProgressDialog progressDialog;
+        Socket socket = null;
+        ObjectOutputStream out = null;
+        ObjectInputStream in = null;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(NotificationsActivity.this,
+                    "Getting all notifications...",
+                    "Connecting to server...");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                socket = new Socket(NetInfo.getServer_ip(),NetInfo.getServer_port());
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject("REQUEST_NOTIFICATIONS");
+                out.writeObject(MainActivity.username);
+                // get offers list
+                notifications = (ArrayList<Notification>) in.readObject();
+                //TODO get server's last response for request_notifications
+
+            }catch(IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+            progressDialog.dismiss();
+            adapter = new NotificationsAdapter(notifications, NotificationsActivity.this);
+            recycler.setAdapter(adapter);
+            // TODO show message if empty
+        }
     }
 }
