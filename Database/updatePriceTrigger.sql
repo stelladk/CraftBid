abae7cc8731a74728c18ddc9db4869e77cf4f61c
themelinaKz <melinas792@gmail.com>
2021-06-02 15:04:31 +0300

    Trigger for updating listing price if max offer is deleted

--- Database/updatePriceTrigger.sql
+++ Database/updatePriceTrigger.sql
@@ -1,3 +1,4 @@
+-- after offer insertion update price of listing
 USE [craftbid]
 GO
 CREATE TRIGGER updatePriceTrigger on Offer
@@ -6,5 +7,22 @@ UPDATE Listing
 SET Listing.min_price = INSERTED.price
 FROM INSERTED
 WHERE Listing.id = inserted.submitted_for
+END
+GO
+
+-- after offer deletion update price of listing with MAX price offered
+USE [craftbid]
+GO
+CREATE TRIGGER updatePriceDeletionTrigger on Offer
+AFTER DELETE AS BEGIN
+UPDATE Listing
+SET Listing.min_price = MAXPRICE
+FROM Listing
+JOIN (
+	SELECT submitted_for, MAX(price) as MAXPRICE
+	FROM Offer
+	GROUP BY submitted_for
+) AS UPDATEDOFFERS
+ON Listing.id = UPDATEDOFFERS.submitted_for
 END
 GO
