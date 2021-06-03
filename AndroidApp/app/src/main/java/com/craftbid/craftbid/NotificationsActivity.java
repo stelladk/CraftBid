5cf23e43721e06ec89dfcb1bb14f008c42dd28f1
themelinaKz <melinas792@gmail.com>
2021-06-03 16:35:16 +0300

    Notifications activity with server connection, empty list message added

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/NotificationsActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/NotificationsActivity.java
@@ -1,42 +1,34 @@
 package com.craftbid.craftbid;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-import com.craftbid.craftbid.adapters.FeedRecyclerAdapter;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.craftbid.craftbid.adapters.NotificationsAdapter;
-import com.craftbid.craftbid.adapters.OffersRecyclerAdapter;
 import com.craftbid.craftbid.model.Notification;
-import com.craftbid.craftbid.model.Offer;
-import com.craftbid.craftbid.model.Thumbnail;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class NotificationsActivity extends AppCompatActivity {
     private NotificationsAdapter adapter;
     private RecyclerView recycler;
     private ArrayList<Notification> notifications;
+    private TextView empty_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,32 +43,41 @@ public class NotificationsActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        empty_message = findViewById(R.id.empty_msg);
         recycler = findViewById(R.id.notifications_recyclerview);
         RecyclerView.LayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recycler.setLayoutManager(manager);
+        new LoadNotificationsTask().execute();
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                goBack();
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            goBack();
         }
+        return super.onOptionsItemSelected(item);
     }
     private void goBack() {
         Intent main = new Intent(NotificationsActivity.this, MainActivity.class);
         startActivity(main);
     }
 
+    /** Shows message if notification list is empty */
+    private void toggleEmptyMessage(){
+        if(adapter.getItemCount() == 0){
+            empty_message.setVisibility(View.VISIBLE);
+        }else{
+            empty_message.setVisibility(View.GONE);
+        }
+    }
+
+    /** on Continue button click, go to Purchase activity*/
     public void purchase(Notification notification) {
         Intent purchase = new Intent(NotificationsActivity.this, PurchaseActivity.class);
         purchase.putExtra("listing_id", notification.getListing_id());
         startActivity(purchase);
     }
 
-    // TODO waiting for server's correction to be tested
     /** On creation of screen, connects to server to get list of Offers for particular listing*/
     private class LoadNotificationsTask extends AsyncTask<Void, Void, Void> {
         ProgressDialog progressDialog;
@@ -101,8 +102,6 @@ public class NotificationsActivity extends AppCompatActivity {
                 out.writeObject(MainActivity.username);
                 // get offers list
                 notifications = (ArrayList<Notification>) in.readObject();
-                //TODO get server's last response for request_notifications
-
             }catch(IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
@@ -121,7 +120,7 @@ public class NotificationsActivity extends AppCompatActivity {
             progressDialog.dismiss();
             adapter = new NotificationsAdapter(notifications, NotificationsActivity.this);
             recycler.setAdapter(adapter);
-            // TODO show message if empty
+            toggleEmptyMessage();
         }
     }
 }
