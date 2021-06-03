045daf7c5d730d4f45498afb42ea4e87cef1fb09
themelinaKz <melinas792@gmail.com>
2021-06-03 18:15:53 +0300

    Load purchase screen done

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/PurchaseActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/PurchaseActivity.java
@@ -1,6 +1,12 @@
 package com.craftbid.craftbid;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,17 +20,31 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.craftbid.craftbid.model.Listing;
+import com.craftbid.craftbid.model.Reward;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class PurchaseActivity extends AppCompatActivity {
+    private int listing_id;
+    private TextView purchase_location;
+    private Listing listing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
 
         Bundle b = getIntent().getExtras();
-        int listing_id;
         if(b!=null){
             listing_id = b.getInt("listing_id");
         }
+        purchase_location = findViewById(R.id.purchase_location);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,10 +52,7 @@ public class PurchaseActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        // TODO get item to be purchased (delivery options are needed)
-        String delivery = "χέρι-με-χέρι";
-
-        availableDeliveryOptions(delivery);
+        new LoadDeliveryOptionsTask().execute();
     }
 
     /** Disables unavailable delivery option if any*/
@@ -88,12 +105,10 @@ public class PurchaseActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                goBack();
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            goBack();
         }
+        return super.onOptionsItemSelected(item);
     }
     private void goBack() {
         Intent main = new Intent(PurchaseActivity.this, MainActivity.class);
@@ -102,5 +117,62 @@ public class PurchaseActivity extends AppCompatActivity {
 
     public void openMain(View view) {
         goBack();
+    }
+
+    /** Loads delivery options and location on screen creation */
+    private class LoadDeliveryOptionsTask extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog progressDialog;
+        private Socket socket = null;
+        private ObjectOutputStream out = null;
+        private ObjectInputStream in = null;
+        private final String resultmsg = "Προέκυψε σφάλμα.";
+        private boolean is_successful = true;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(PurchaseActivity.this,
+                    "Load delivery options...",
+                    "Connecting to server...");
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                socket = new Socket(NetInfo.getServer_ip(), NetInfo.getServer_port());
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject("VIEW_LISTING");
+                out.writeObject(listing_id);
+                listing = (Listing) in.readObject();
+                if(listing==null) is_successful=false;
+
+                // non needed info
+                in.readObject(); //thumbnail
+                in.readObject(); //photos
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+                is_successful = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressDialog.dismiss();
+            try {
+                socket.close();
+                out.close();
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(!is_successful) {
+                Snackbar.make(getWindow().getDecorView().getRootView(), resultmsg, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                goBack();
+            }
+            purchase_location.setText(listing.getLocation());
+            availableDeliveryOptions(listing.getDelivery());
+        }
     }
 }
