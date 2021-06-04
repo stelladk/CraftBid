82646373a5e630a29a43cc0c3c14e36e9173a4a1
themelinaKz <melinas792@gmail.com>
2021-06-04 19:00:01 +0300

    Login backend completed (in comments for debugging)

--- AndroidApp/app/src/main/java/com/craftbid/craftbid/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/craftbid/craftbid/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.craftbid.craftbid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class LoginActivity extends AppCompatActivity {
-    private EditText username_edit;
+    private EditText username_edit, password_edit;
+    private boolean creator;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         username_edit = findViewById(R.id.username);
+        password_edit = findViewById(R.id.password);
     }
 
     public void openSignupCustomer(View view) {
@@ -38,13 +49,78 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void attemptLogin(View view) {
-        //TODO
-        String username = username_edit.getText().toString();
-        Log.d("LOGIN", "onCreate: username("+username+")");
-        //Do actions to confirm login
-        Intent main = new Intent(LoginActivity.this, MainActivity.class);
-        main.putExtra("username", username); //Send his username
-        main.putExtra("creator", true); //TODO check if user is creator
-        startActivity(main);
+        username = username_edit.getText().toString();
+        String password = password_edit.getText().toString();
+        //TODO after debug uncomment new AttemptLoginTask().execute(username, password);
+    }
+
+    /** Connects to server to verify user's credentials */
+    private class AttemptLoginTask extends AsyncTask<String, Void, Void> {
+        ProgressDialog progressDialog;
+        Socket socket = null;
+        ObjectOutputStream out = null;
+        ObjectInputStream in = null;
+        String response, resultmsg;
+        boolean is_successful = false;
+
+        @Override
+        protected Void doInBackground(String... credentials) {
+            try {
+                socket = new Socket(NetInfo.getServer_ip(),NetInfo.getServer_port());
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject("LOGIN");
+                out.writeObject(credentials[0]); //username
+                out.writeObject(credentials[1]); //password
+
+                response = (String) in.readObject();
+                switch (response) {
+                    case "LOGIN SUCCESSFUL":
+                        creator = (boolean) in.readObject();
+                        is_successful = true;
+                        break;
+                    case "WRONG PASSWORD":
+                        resultmsg = "Λάθος κωδικός.";
+                        break;
+                    case "WRONG USERNAME":
+                        resultmsg = "Το username δεν υπάρχει.";
+                        break;
+                }
+            }catch(IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(LoginActivity.this,
+                    "Verifying given credentials...",
+                    "Connecting to server...");
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressDialog.dismiss();
+            try {
+                socket.close();
+                out.close();
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // proceed to main screen
+            if(is_successful) {
+                Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                main.putExtra("username", username);
+                main.putExtra("creator", creator);
+                startActivity(main);
+            }
+            else {
+                Snackbar.make( getWindow().getDecorView().getRootView(), resultmsg , Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }
     }
 }
