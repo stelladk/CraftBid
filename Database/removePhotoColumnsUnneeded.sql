449949ae30d91ad9068d8fe4385ff0a1905a1dfa
Themelina Kouzoumpasi <32911681+themelinaKz@users.noreply.github.com>
2021-06-06 00:58:19 +0300

    Delete removePhotoColumnsUnneeded.sql

--- Database/removePhotoColumnsUnneeded.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE Listing DROP COLUMN thumbnail;
-
-ALTER TABLE Notification DROP COLUMN photo;
-
-ALTER TABLE Reward DROP COLUMN photo;
-
-ALTER TABLE UserInfo DROP COLUMN photo;
-
---TODO remove table Photo
